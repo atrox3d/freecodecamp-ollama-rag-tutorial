8ce59b4b5780c14bacbd00b4e80c5c785129f582
atrox3d <atrox3d@gmail.com>
2025-02-11 14:19:54 +0100

    add typer

--- basic.py
+++ basic.py
@@ -1,25 +1,42 @@
-from unittest import result
-from urllib import response
 import requests
 import json
+import typer
 
-url = 'http://localhost:11434/api/generate'
 
-data = {
-    'model': 'llama3.2',
-    'prompt': 'tell me a short story and make it funny',
-    
-}
+SERVER = 'localhost'
+PORT = 11434
+ENDPOINT = 'api/generate'
+URL = f'http://{SERVER}:{PORT}/{ENDPOINT}'
 
-response = requests.post(url, json=data, stream=True)
+app = typer.Typer()
 
-if response.status_code == 200:
-    print('Generated text:', end='', flush=True)
-    for line in response.iter_lines():
-        if line:
-            decoded_line = line.decode('utf-8')
-            result = json.loads(decoded_line)
-            generated_text = result.get('response', '')
-            print(generated_text, end='', flush=True)
-else:
-    print('Error:', response.status_code)
+@app.command()
+def chat(
+    prompt  :str,
+    server  :str = SERVER,
+    port    :int = PORT,
+    endpoint:str = ENDPOINT,
+    model   :str = 'llama3.2',
+):
+    data = {
+        'model': model,
+        'prompt': prompt,
+    }
+
+    url = f'http://{server}:{port}/{endpoint}'
+    response = requests.post(url, json=data, stream=True)
+
+    if response.status_code == 200:
+        print('Generated text:', end='', flush=True)
+        for line in response.iter_lines():
+            if line:
+                decoded_line = line.decode('utf-8')
+                result = json.loads(decoded_line)
+                generated_text = result.get('response', '')
+                print(generated_text, end='', flush=True)
+    else:
+        print('Error:', response.status_code)
+
+
+if __name__ == "__main__":
+    app()
