89d4e76dfc2080b8d30351d779f687ab77507899
atrox3d <atrox3d@gmail.com>
2025-02-12 09:55:42 +0100

    restore example section

--- api.py
+++ api.py
@@ -6,49 +6,51 @@ from ollamaserver import start_ollama, stop_ollama, ollama_up
 
 start_ollama()
 
-if False:
-    # print installed models
-    response:ollama.ListResponse = ollama.list()
-    # print([model['model']for model in response.model_dump()['models']])
-    # print([model.model for model in response.models])
+# print installed models
+response:ollama.ListResponse = ollama.list()
+# print([model['model']for model in response.model_dump()['models']])
+# print([model.model for model in response.models])
 
 
-    res = ollama.chat(
-        model='llama3.2',
-        messages=[
-            {
-                'role': 'user',
-                'content': 'Hello, how are you?'
-            },
-        ],
-        stream=True
-    )
+res = ollama.chat(
+    model='llama3.2',
+    messages=[
+        {
+            'role': 'user',
+            'content': 'Hello, how are you?'
+        },
+    ],
+    stream=True
+)
 
-    # to use the old dict interface use model_dump()
-    # print(res.model_dump()['message']['content'])
-    # print(res.message.content)
-    for chunk in res:
-        print(chunk.message.content, end='', flush=True)
-    print()
+# to use the old dict interface use model_dump()
+# print(res.model_dump()['message']['content'])
+# print(res.message.content)
+for chunk in res:
+    print(chunk.message.content, end='', flush=True)
+print()
 
 
-    res = ollama.generate(
-        model='llama3.2',
-        prompt='Hello, how are you?',
-        stream=True
-    )
-    for chunk in res:
-        print(chunk.response, end='', flush=True)
-    print()
+res = ollama.generate(
+    model='llama3.2',
+    prompt='Hello, how are you?',
+    stream=True
+)
+for chunk in res:
+    print(chunk.response, end='', flush=True)
+print()
 
-    show = ollama.show('llama3.2')
-    print(show.modelinfo)
+show = ollama.show('llama3.2')
+print(show.modelinfo)
 
+#####################################################
+# this does not work anymore, the api has changed
 modelfile= '''
 FROM llama3.2:latest
 SYSTEM you are a cat, you answer only by meowing, purring and hissing
 PARAMETER temperature 0.1
 '''
+#####################################################
 
 res = ollama.create(
         model='cat', 
