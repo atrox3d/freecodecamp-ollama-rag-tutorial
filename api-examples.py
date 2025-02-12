fa09fdf4fa23e50874f0c69bfaca8806177cc61a
atrox3d <atrox3d@gmail.com>
2025-02-12 10:03:56 +0100

    add some comment about compatibility break of this ollama version 0.4.7

--- api-examples.py
+++ api-examples.py
@@ -6,12 +6,19 @@ from ollamaserver import start_ollama, stop_ollama, ollama_up
 
 start_ollama()
 
+######################################################################
 # print installed models
+######################################################################
+
 response:ollama.ListResponse = ollama.list()
 # print([model['model']for model in response.model_dump()['models']])
 # print([model.model for model in response.models])
 
 
+######################################################################
+# create chat
+######################################################################
+# chat keeps a context and history
 res = ollama.chat(
     model='llama3.2',
     messages=[
@@ -23,14 +30,22 @@ res = ollama.chat(
     stream=True
 )
 
+#*************************************************
+# changed in ollama at least 0.4.7
+#*************************************************
 # to use the old dict interface use model_dump()
 # print(res.model_dump()['message']['content'])
 # print(res.message.content)
+#*************************************************
 for chunk in res:
     print(chunk.message.content, end='', flush=True)
 print()
 
 
+######################################################################
+# create response
+######################################################################
+# generate does not keep a context
 res = ollama.generate(
     model='llama3.2',
     prompt='Hello, how are you?',
@@ -43,15 +58,21 @@ print()
 show = ollama.show('llama3.2')
 print(show.modelinfo)
 
-#####################################################
+######################################################################
+# create model
+######################################################################
+
+#*************************************************
 # this does not work anymore, the api has changed
+#*************************************************
 modelfile= '''
 FROM llama3.2:latest
 SYSTEM you are a cat, you answer only by meowing, purring and hissing
 PARAMETER temperature 0.1
 '''
-#####################################################
+#*************************************************
 
+# new create interface for version 0.4.7
 res = ollama.create(
         model='cat', 
         from_='llama3.2',
@@ -66,4 +87,6 @@ print(res.response)
 ollama.delete('cat')
 models = ollama.list()
 print([model.model for model in models.models])
+
+
 stop_ollama()
