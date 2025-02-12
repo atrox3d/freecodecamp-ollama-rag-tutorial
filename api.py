926e94cdddd5a63aef6754668cd809ae259c814a
atrox3d <atrox3d@gmail.com>
2025-02-12 09:51:09 +0100

    update tutorial example with new api params for api/create (does not accept a modelfile anymore

--- api.py
+++ api.py
@@ -5,41 +5,63 @@ from ollamaserver import start_ollama, stop_ollama, ollama_up
 
 
 start_ollama()
-# print installed models
-response:ollama.ListResponse = ollama.list()
-# print([model['model']for model in response.model_dump()['models']])
-# print([model.model for model in response.models])
+
+if False:
+    # print installed models
+    response:ollama.ListResponse = ollama.list()
+    # print([model['model']for model in response.model_dump()['models']])
+    # print([model.model for model in response.models])
 
 
-res = ollama.chat(
-    model='llama3.2',
-    messages=[
-        {
-            'role': 'user',
-            'content': 'Hello, how are you?'
-        },
-    ],
-    stream=True
+    res = ollama.chat(
+        model='llama3.2',
+        messages=[
+            {
+                'role': 'user',
+                'content': 'Hello, how are you?'
+            },
+        ],
+        stream=True
+    )
+
+    # to use the old dict interface use model_dump()
+    # print(res.model_dump()['message']['content'])
+    # print(res.message.content)
+    for chunk in res:
+        print(chunk.message.content, end='', flush=True)
+    print()
+
+
+    res = ollama.generate(
+        model='llama3.2',
+        prompt='Hello, how are you?',
+        stream=True
+    )
+    for chunk in res:
+        print(chunk.response, end='', flush=True)
+    print()
+
+    show = ollama.show('llama3.2')
+    print(show.modelinfo)
+
+modelfile= '''
+FROM llama3.2:latest
+SYSTEM you are a cat, you answer only by meowing, purring and hissing
+PARAMETER temperature 0.1
+'''
+
+res = ollama.create(
+        model='cat', 
+        from_='llama3.2',
+        system='you are a cat, you answer only by meowing, purring and hissing',
+        parameters={'temperature':0.1}
 )
+print(res)
 
-# to use the old dict interface use model_dump()
-# print(res.model_dump()['message']['content'])
-# print(res.message.content)
-for chunk in res:
-    print(chunk.message.content, end='', flush=True)
-print()
+res = ollama.generate(model='cat', prompt='hello')
+print(res.response)
 
-
-res = ollama.generate(
-    model='llama3.2',
-    prompt='Hello, how are you?',
-    stream=True
-)
-for chunk in res:
-    print(chunk.response, end='', flush=True)
-print()
-
-show = ollama.show('llama3.2')
-print(show.modelinfo)
-
+ollama.delete('cat')
+models = ollama.list()
+print([model.model for model in models.models])
 stop_ollama()
